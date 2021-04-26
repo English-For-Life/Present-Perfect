13938ac242a1258a1d0d19195f321cd360dfb661
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-04-26 19:53:12 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,155 @@
+//Array of objects
+const quiz = [
+    {
+        q:"My car ___ down.",
+        options:["has broke", "has broken", "have broken", "have broke"],
+        answer: 1
+    },
+    {
+     q:"They ___ all my questions.",
+        options:["have answered", "has answered", "have answer", "have answerd"],
+        answer: 0   
+    },
+    {
+        q:"I ___ Mike fo 10 years.",
+        options:["has known", "have knowen", "have known", "has knowen"],
+        answer: 2
+    },
+    {
+        q:"She ___ her keys.",
+        options:["has lose", "have lose", "have lost", "has lost"],
+        answer: 3
+    },
+    {
+        q:"The cat ___ all of its milk.",
+        options:["have drunk", "has drank", "has drunk", "have drank"],
+        answer: 2
+    },
+    {
+        q:"We ___ to Canada two times.",
+        options:["has been", "have been", "have being", "has being"],
+        answer: 1
+    },
+    {
+        q:"Where is Mike? - He ___ for a walk.",
+        options:["has gone", "has went", "have gone", "have went"],
+        answer: 0
+    },
+    {
+        q:"The students ___ their test.",
+        options:["has finishen", "has finished", "have finishen", "have finished"],
+        answer: 3
+    },
+    {
+        q:"The lesson ___ .",
+        options:["has began", "has begun", "have begun", "have began"],
+        answer: 1
+    },
+    {
+        q:"I ___ my mobile phone somewhere but I can't remember where.",
+        options:["have leave", "has left", "have left", "have leaved"],
+        answer: 2
+    },
+    {
+        q:"The teacher ___ yet.",
+        options:["hasn't came", "hasn't come", "haven't come", "haven't came"],
+        answer: 1
+    },
+    {
+     q:"I hope he ___ a mistake.",
+        options:["hasn't made", "haven't made", "hasn't make", "haven't maden"],
+        answer: 0   
+    },
+    {
+        q:"We ___ them for two years.",
+        options:["haven't meet", "hasn't met", "haven't met", "hasn't meet"],
+        answer: 2
+    },
+    {
+        q:"I ___ such delicious food for ages.",
+        options:["hasn't ate", "haven't ate", "hasn't eaten", "haven't eaten"],
+        answer: 3
+    },
+    {
+        q:"He ___ his keys yet.",
+        options:["hasn't find", "haven't found", "hasn't found", "haven't find"],
+        answer: 2
+    },
+    {
+        q:"She ___ the flowers.",
+        options:["hasn't waterd", "hasn't watered", "haven't watered", "haven't waterd"],
+        answer: 1
+    },
+    {
+        q:"They ___ their flat yet.",
+        options:["haven't cleaned", "haven't cleand", "hasn't cleaned", "hasn't cleand"],
+        answer: 0
+    },
+    {
+        q:"Daniel ___ his car yet.",
+        options:["hasn't wash", "haven't wash", "haven't washed", "hasn't washed"],
+        answer: 3
+    },
+    {
+        q:"We ___ her at school.",
+        options:["haven't saw", "haven't seen", "hasn't saw", "hasn't seen"],
+        answer: 1
+    },
+    {
+        q:"Ann ___ her grandparents yet.",
+        options:["hasn't visit", "haven't visited", "hasn't visited", "haven't visit"],
+        answer: 2
+    },
+    {
+        q:"___ you ___ dinner yet?",
+        options:["Have / have", "Have / had", "Has / had", "Has / have"],
+        answer: 1
+    },
+    {
+     q:"Where ___ you ___ this book?",
+        options:["have / found", "has / found", "have / find", "has / find"],
+        answer: 0   
+    },
+    {
+        q:"___ he ___ this film yet?",
+        options:["Have / seen", "Has / saw", "Has / seen", "Have / saw"],
+        answer: 2
+    },
+    {
+        q:"___ I ___ you before?",
+        options:["Has / meet", "Has / met", "Have / meet", "Have / met"],
+        answer: 3
+    },
+    {
+        q:"Where ___ you ___ ?",
+        options:["has / been", "have / were", "have / been", "has / was"],
+        answer: 2
+    },
+    {
+        q:"___ they ___ to the USA?",
+        options:["Has / gone", "Have / gone", "Has / went", "Have / went"],
+        answer: 1
+    },
+    {
+        q:"___ you ___ this dress yourself?",
+        options:["Have / made", "Have / make", "Has / made", "Has / make"],
+        answer: 0
+    },
+    {
+        q:"She ___ late for a meeting?",
+        options:["have / be", "has / be", "have / been", "has / been"],
+        answer: 3
+    },
+    {
+        q:"How much coffee ___ you ___ yet?",
+        options:["have / drank", "have / drunk", "has / drunk", "has / drank"],
+        answer: 1
+    },
+    {
+        q:"___ you ___ your homework yet?",
+        options:["Have / did", "Has / done", "Have / done", "Has / did"],
+        answer: 2
+    }
+
+
+]
